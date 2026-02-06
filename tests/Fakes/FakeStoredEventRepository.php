fb8ec661b3220a3babfd9a0997ad467a632e8793
Joel Butcher <joelbutcher@users.noreply.github.com>
2026-02-06 17:50:40 +0000

    fix

--- tests/Fakes/FakeStoredEventRepository.php
+++ tests/Fakes/FakeStoredEventRepository.php
@@ -61,7 +61,7 @@ class FakeStoredEventRepository implements StoredEventRepository
         $this->countsStartingFrom[$startingFrom] = $count;
     }
 
-    public function find(int $id): StoredEvent;
+    public function find(int $id): StoredEvent
     {
         // TODO: Implement find() method.
     }
